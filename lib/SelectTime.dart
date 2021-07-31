7c72dba650f52c5cf38283d192ba41ec9eace8e9
thtkiet19 <63939319+thtkiet19@users.noreply.github.com>
2021-07-31 23:45:56 +0700

    nothing new

--- lib/SelectTime.dart
+++ lib/SelectTime.dart
@@ -3,102 +3,104 @@ import 'package:numberpicker/numberpicker.dart';
 
 //TODO: clean up the widgets
 class SelectTimer extends StatefulWidget {
-  // late final int timestampe;
-  // SelectTimer({required Key Key, required this.timestampe});
-
   @override
   _SelectTimerState createState() => _SelectTimerState();
 }
 
 class _SelectTimerState extends State<SelectTimer> {
-  int _currentHours = 0;
-
-  int _currentMinutes = 45;
-  int timestampe = 0;
+  void loadtime() {}
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 1,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: Center(
-            child: Text('Pomodoro'),
-          ),
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Center(
+          child: Text('Pomodoro'),
         ),
-        body: TabBarView(
-          children: [
-            Container(
-              color: Colors.blueGrey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        children: <Widget>[
-                          Text('Hours', style: TextStyle(color: Colors.white)),
-                          NumberPicker(
-                            textStyle: TextStyle(color: Colors.white),
-                            value: _currentHours,
-                            minValue: 0,
-                            maxValue: 60,
-                            step: 1,
-                            haptics: true,
-                            selectedTextStyle:
-                                TextStyle(color: Colors.teal, fontSize: 30),
-                            decoration: BoxDecoration(
-                              border: Border(
-                                  top: BorderSide(), bottom: BorderSide()),
-                            ),
-                            onChanged: (value) =>
-                                setState(() => _currentHours = value),
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 15),
-                      Column(
-                        children: <Widget>[
-                          Text('Minutes',
-                              style: TextStyle(color: Colors.white)),
-                          NumberPicker(
-                            textStyle: TextStyle(color: Colors.white),
-                            value: _currentMinutes,
-                            selectedTextStyle:
-                                TextStyle(color: Colors.teal, fontSize: 30),
-                            minValue: 0,
-                            maxValue: 60,
-                            decoration: BoxDecoration(
-                              border: Border(
-                                  top: BorderSide(), bottom: BorderSide()),
-                            ),
-                            step: 1,
-                            haptics: true,
-                            onChanged: (value) =>
-                                setState(() => _currentMinutes = value),
-                          ),
-                        ],
-                      )
-                    ],
+      ),
+      body: Container(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        timepicker(),
+      ])),
+    );
+  }
+}
+
+class timepicker extends StatefulWidget {
+  int timestampe = 0;
+  @override
+  _timepickerState createState() => _timepickerState();
+}
+
+class _timepickerState extends State<timepicker> {
+  int _currentHours = 0;
+  int _currentMinutes = 45;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: <Widget>[
+                  Text('Hours', style: TextStyle(color: Colors.white)),
+                  NumberPicker(
+                    textStyle: TextStyle(color: Colors.white),
+                    value: _currentHours,
+                    minValue: 0,
+                    maxValue: 9,
+                    step: 1,
+                    haptics: false,
+                    selectedTextStyle:
+                        TextStyle(color: Colors.teal, fontSize: 30),
+                    decoration: BoxDecoration(
+                      border: Border(top: BorderSide(), bottom: BorderSide()),
+                    ),
+                    onChanged: (value) => setState(() => _currentHours = value),
                   ),
-                  TextButton(
-                    onPressed: search(),
-                    child: Text('begin'),
-                  )
                 ],
               ),
-            ),
-          ],
-        ),
+              SizedBox(width: 15),
+              Column(
+                children: <Widget>[
+                  Text('Minutes', style: TextStyle(color: Colors.white)),
+                  NumberPicker(
+                    textStyle: TextStyle(color: Colors.white),
+                    value: _currentMinutes,
+                    selectedTextStyle:
+                        TextStyle(color: Colors.teal, fontSize: 30),
+                    minValue: 0,
+                    maxValue: 60,
+                    decoration: BoxDecoration(
+                      border: Border(top: BorderSide(), bottom: BorderSide()),
+                    ),
+                    step: 1,
+                    haptics: false,
+                    onChanged: (value) =>
+                        setState(() => _currentMinutes = value),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: addtime,
+            icon: Icon(Icons.add_alarm_sharp),
+          ),
+        ],
       ),
     );
   }
 
-  search() {
-    timestampe = _currentHours * 60 + _currentMinutes;
-    Navigator.pushReplacementNamed(context, '/home',
-        arguments: {'timestampe': timestampe});
+  void addtime() {
+    widget.timestampe = _currentHours * 60 + _currentMinutes;
+    Navigator.pushNamed(context, '/home', arguments: {
+      'time': widget.timestampe,
+    });
   }
 }
--- lib/pomodoro.dart
+++ lib/pomodoro.dart
@@ -1,9 +1,74 @@
+import 'dart:async';
+
+import 'package:custom_timer/custom_timer.dart';
 import 'package:flutter/material.dart';
 
-class pomodoro extends StatelessWidget {
+class pomodoro extends StatefulWidget {
   const pomodoro({Key? key}) : super(key: key);
+
   @override
+  _pomodoroState createState() => _pomodoroState();
+}
+
+class _pomodoroState extends State<pomodoro> {
+  Map data = {};
+  late Timer _timer;
+  late int _start;
+  void startTimer() {
+    print('STARTED TIMER');
+    const oneSec = const Duration(seconds: 1);
+    _timer = new Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        if (_start == 0) {
+          setState(() {
+            timer.cancel();
+          });
+        } else {
+          setState(() {
+            _start--;
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return Container(child: Text('hello'));
+    data =
+        data.isEmpty ? ModalRoute.of(context)!.settings.arguments as Map : data;
+    _start = data['time'] * 60;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Center(
+          child: Text('Pomodoro'),
+        ),
+      ),
+      body: Container(
+          color: Colors.blueGrey,
+          child: Center(
+            child: CustomTimer(
+              from: Duration(minutes: data['time']),
+              to: Duration(minutes: 0),
+              onBuildAction: CustomTimerAction.auto_start,
+              builder: (CustomTimerRemainingTime remaining) {
+                return Text(
+                  "${remaining.hours}:${remaining.minutes}:${remaining.seconds}",
+                  style: TextStyle(fontSize: 30.0),
+                );
+              },
+            ),
+          )),
+    );
+  }
+
+  void back() {
+    Navigator.pop(context);
   }
 }
